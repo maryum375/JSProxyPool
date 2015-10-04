c1b377d5d6864466e117f4af8604fbf30094432f
Ran <maryum375@gmail.com>
2015-10-04 14:45:39 +0300

    Added active property to proxy object

--- Proxy.js
+++ Proxy.js
@@ -2,6 +2,8 @@ function Proxy(address, port) {
     this._address = address;
     this._port = port;
 
+    this._active = true;
+
     /* This indicates on the last time (timestamp) this proxy was used */
     this._lastUsedTime = 0;
 }
@@ -10,12 +12,17 @@ Proxy.prototype.toString = function () {
     return this._address + ":" + this._port;
 };
 
-Proxy.compare = function (proxy1,proxy2) {
+Proxy.compare = function (proxy1, proxy2) {
     if (proxy1._lastUsedTime < proxy2._lastUsedTime)
         return -1;
     if (proxy1._lastUsedTime > proxy2._lastUsedTime)
         return 1;
     return 0;
-}
+};
+
+Object.isProxy = function (obj) {
+    return (obj.hasOwnProperty("_address") && obj.hasOwnProperty("_port"));
+
+};
 
 module.exports = Proxy;
